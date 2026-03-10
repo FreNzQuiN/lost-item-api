c399b6b640c8e9a3b49843ee194bad7affc8ad8d
Syauqi-Sy <syauqi.sy926@gmail.com>
2026-03-10 14:32:49 +0700

    tambah routing

--- routes/api.php
+++ routes/api.php
@@ -1,3 +1,7 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LostItemController;
 
 Route::apiResource('lost-items', LostItemController::class);
